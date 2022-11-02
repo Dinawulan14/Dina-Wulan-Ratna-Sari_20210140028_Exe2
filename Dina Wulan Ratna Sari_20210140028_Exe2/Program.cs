331dc4e211b871c885e4fd559fb735d4b1f432dd
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-11-02 16:05:18 +0700

    Menambahkan While untuk mendapatkan jumlah elemen pada array

--- Dina Wulan Ratna Sari_20210140028_Exe2/Program.cs
+++ Dina Wulan Ratna Sari_20210140028_Exe2/Program.cs
@@ -8,5 +8,23 @@ namespace Dina_Wulan_Ratna_Sari_20210140028_Exe2
 {
     class Program
     {
-
+        //definisikan array dengan ukuran maksimum 48
+        private int[] arr = new int[48];
+        //variable ukuran array
+        private int n;
+        //fungsi untuk menginputkan ukuran array
+        public void read()
+        {
+            while (true)
+            {
+                Console.WriteLine("Masukkan jumlah Element/Data Array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 48)
+                    break;
+                else
+                    Console.WriteLine("\nArray maksimum 48 element/data. \n");
+            }
+            }
+        }
     }
