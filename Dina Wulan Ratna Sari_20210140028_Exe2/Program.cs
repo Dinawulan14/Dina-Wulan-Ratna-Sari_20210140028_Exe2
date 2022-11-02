611baf305658181723997b13ea9cc402ab9e6c0a
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2022-11-02 15:47:53 +0700

    Add project files.

---
+++ Dina Wulan Ratna Sari_20210140028_Exe2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dina_Wulan_Ratna_Sari_20210140028_Exe2
+{
+    class Program
+    {
+        //definisikan array dengan ukuran maksimum 48
+        private int[] a = new int[48];
+        //variable ukuran array
+        private int n;
+        //fungsi untuk menginputkan ukuran array
+        public void read()
+        {
+
+        }
+    }
